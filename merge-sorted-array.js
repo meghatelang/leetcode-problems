a9461e3ebe52db353116bd4026b69c3597d7f44f
Megha Telang <59637832+meghatelang@users.noreply.github.com>
2021-06-24 11:11:51 +0530

    Add files via upload

---
+++ merge-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for(let i = m+n-1; i >= m; i--){
+        nums1[i] = nums2.pop()
+    }
+    nums1.sort((a,b) => a-b);
+};
